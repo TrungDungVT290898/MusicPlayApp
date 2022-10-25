a4e7b7575a65fe61935cd1aa3569a46c5cec1a4a
unknown <nguyentrungdung12a@gmail.com>
2022-10-25 16:39:45 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { MusicPlayerProvider } from "./contexts/MusicPlayerContext";
-import TrackList from "./components/TrackList";
-import Controller from "./components/Controller";
 import AppContainer from "./components/AppContainer";
 const App = () => {
   return (
